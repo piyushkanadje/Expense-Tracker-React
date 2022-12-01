a153259295e73c9dba2e8029f64f0527c8d3a24f
KARMASPHERE <piyushkanadje@gmail.com>
2022-12-01 11:55:26 -0800

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
-import ExpenseItem from "./components/ExpenseItem";
 import "./index.css"
+import './components/Expenses.css'
+import Card from './components/Card';
+import Expenses  from "./components/Expenses";
+
 function App() {
   const expenses = [
     {
@@ -24,14 +27,11 @@ function App() {
     },
   ];
   return (
-    <div >
-      <h2 className="name">This is me Piyush</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}> </ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-      
-    </div>
+
+    <Card className="expenses">
+      <h2 className="name">ExpenseTracker</h2>
+      <Expenses items={expenses} />
+    </Card>
   );
 }
 
